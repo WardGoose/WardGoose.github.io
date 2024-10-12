c3ab7960721b11056eaf7a6e014a55035987e671
Hyeonseok-Cho <pozuhtuhv@gmail.com>
2024-10-12 10:46:56 +0900

    (edit) file_path

--- py/multi_price.py
+++ py/multi_price.py
@@ -4,26 +4,10 @@
 # ethereum
 # 0x28561b8a2360f463011c16b6cc0b0cbef8dbbcad
 
-# Shib inu
+# Shiba inu
 # ethereum
 # 0x95ad61b0a150d79219dcf64e1e6cc01f0b64c4ce
 
-# # ======
-# One Result
-# import requests
-# chainID = 'ethereum'
-# pairId = '0x28561b8a2360f463011c16b6cc0b0cbef8dbbcad'
-
-# response = requests.get(
-#     "https://api.dexscreener.com/latest/dex/pairs/{chainId}/{pairId}",
-#     headers={},
-# )
-
-# data = response.json()
-# # ======
-
-# ======
-# Multi Result
 import requests
 import json
 
@@ -36,11 +20,12 @@ response = requests.get(
 )
 data = response.json()
 
-with open('history.json', 'w', encoding='utf-8') as file:
+with open('../history.json', 'w', encoding='utf-8') as file:
     json.dump(data, file, ensure_ascii=False, indent=4)
-    data1 = data['pairs']
-    for i in data1:
-        result = i['labels']
-        if 'v3' in result:
-            print(i['priceUsd'])
-# ======
+
+# # price_load
+#     data1 = data['pairs']
+#     for i in data1:
+#         result = i['labels']
+#         if 'v3' in result:
+#             print(i['priceUsd'])
