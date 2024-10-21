378dd2a27565b6b86c8b0fd1c6fd6d4a067d28a4
Hyeonseok-Cho <pozuhtuhv@gmail.com>
2024-10-21 18:14:14 +0900

    (edit) latest

--- py/multi_price.py
+++ py/multi_price.py
@@ -12,20 +12,25 @@ import requests
 import json
 
 # tokenAddresses = "0x28561b8a2360f463011c16b6cc0b0cbef8dbbcad,0x95ad61b0a150d79219dcf64e1e6cc01f0b64c4ce"
-tokenAddresses = "0x28561b8a2360f463011c16b6cc0b0cbef8dbbcad"
 
-response = requests.get(
-    f"https://api.dexscreener.com/latest/dex/tokens/{tokenAddresses}",
-    headers={},
-)
-data = response.json()
+API_URLS = {
+    'modeng': f"0x28561b8a2360f463011c16b6cc0b0cbef8dbbcad",
+    'shib': f'0x95ad61b0a150d79219dcf64e1e6cc01f0b64c4ce'
+}
 
-with open('py/data/history.json', 'w', encoding='utf-8') as file:
-    json.dump(data, file, ensure_ascii=False, indent=4)
+for filename, token in API_URLS.items():
+    response = requests.get(
+        f"https://api.dexscreener.com/latest/dex/tokens/{token}",
+        headers={},
+    )
+    data = response.json()
 
-# price_load
-    data1 = data['pairs']
-    for i in data1:
-        result = i['labels']
-        if 'v2' in result: # v2, v3
-            print(i['priceUsd'])
+    with open(f'py/data/{filename}.json', 'w', encoding='utf-8') as file:
+        json.dump(data, file, ensure_ascii=False, indent=4)
+
+    # price_load
+        data1 = data['pairs']
+        for i in data1:
+            result = i['labels']
+            if 'v3' in result:  # v2가 있는지 확인
+                print(f"Symbol: {i['baseToken']['symbol']}, Price: {i.get('priceUsd', 'N/A')}")
