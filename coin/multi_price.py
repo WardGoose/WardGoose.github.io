a07bf8618f8888f09a9cbccb08726fd6f7b55fce
Hyeonseok-Cho <pozuhtuhv@gmail.com>
2024-10-22 12:05:58 +0900

    (edit) yml_and_pricesave

--- coin/multi_price.py
+++ coin/multi_price.py
@@ -10,6 +10,7 @@
 
 import requests
 import json
+from datetime import datetime
 
 # tokenAddresses = "0x28561b8a2360f463011c16b6cc0b0cbef8dbbcad,0x95ad61b0a150d79219dcf64e1e6cc01f0b64c4ce"
 
@@ -25,14 +26,26 @@ for filename, token in API_URLS.items():
     )
     data = response.json()
 
-    with open(f'coin/data/{filename}.json', 'w', encoding='utf-8') as file:
-        json.dump(data, file, ensure_ascii=False, indent=4)
-    if filename == 'moodengeth':
-        data1 = data['pairs']
-        for i in data1:
-            result = i['labels']
-            if 'v3' in result:  # v2가 있는지 확인
-                print(f"Symbol: {i['baseToken']['symbol']}, Price: {i.get('priceUsd', 'N/A')}")
-    elif filename == 'moodengsol':
-        data1 = data['pairs'][0]
-        print(f"Symbol: {data1['baseToken']['symbol']}, Price: {data1.get('priceUsd', 'N/A')}")
+    current_time = datetime.now().strftime('%Y-%m-%d %H:%M')
+    
+    with open(f'coin/data/{filename}.txt', 'w', encoding='utf-8') as file:
+        # json.dump(data, file, ensure_ascii=False, indent=4) # json 전체내용 저장
+        if filename == 'moodengeth':
+            data1 = data['pairs']
+            for i in data1:
+                result = i['labels']
+                if 'v3' in result:  # v3가 있는지 확인 , json 내용엔 v2 도 있음
+                    result = f"{current_time} {i.get('priceUsd', 'N/A')}\n"
+                    file.write(result)
+
+                    # print(f"Symbol: {i['baseToken']['symbol']}, Price: {i.get('priceUsd', 'N/A')}") # 단순 결과 출력
+
+        elif filename == 'moodengsol':
+            data1 = data['pairs'][0]
+            result = f"{current_time} {i.get('priceUsd', 'N/A')}\n"
+            file.write(result)
+
+            # print(f"Symbol: {data1['baseToken']['symbol']}, Price: {data1.get('priceUsd', 'N/A')}") # 단순 결과 출력
+
+    
+
--- coin/one_price.py
+++ coin/one_price.py
@@ -21,7 +21,7 @@ response = requests.get(
 
 data = response.json()
 
-with open('coin/data/history.json', 'w', encoding='utf-8') as file:
+with open('coin/data/history.txt', 'w', encoding='utf-8') as file:
     json.dump(data, file, ensure_ascii=False, indent=4)
     
 # # price_load
